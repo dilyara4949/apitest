0aec00d904c9d170273a3935cd650a6767f0c55e
dilya <mukhambetovad49@mail.ru>
2024-05-24 11:54:20 +0500

    add unit test

--- unit_test.go
+++ unit_test.go
@@ -1,17 +1,86 @@
+//go:build unit || all
+// +build unit all
+
 package main
 
-//import "testing"
-//
-//func TestGetUser(t *testing.T) {
-//	//var tests = []struct {
-//	//
-//	//}
-//}
-//
-//func TestAddUser(t *testing.T) {
-//
-//}
-//
-//func TestDeleteUser(t *testing.T) {
-//
-//}
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetUser(t *testing.T) {
+	var tests = []struct {
+		email string
+		name  string
+	}{
+		{"test@gmail.com", "testname"},
+		{"test2@gmail.com", ""},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			users.mu.Lock()
+			users.storage[test.email] = test.name
+			users.mu.Unlock()
+
+			res := GetUser(test.email)
+
+			require.Equal(t, test.name, res)
+		})
+	}
+}
+
+func TestAddUser(t *testing.T) {
+	var tests = []struct {
+		email string
+		name  string
+	}{
+		{"test@gmail.com", "testname"},
+		{"test2@gmail.com", ""},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			users.mu.Lock()
+			users.storage[test.email] = test.name
+			users.mu.Unlock()
+
+			res := GetUser(test.email)
+
+			require.Equal(t, test.name, res)
+		})
+	}
+}
+
+func TestDeleteUser(t *testing.T) {
+	var tests = []struct {
+		email string
+		name  string
+	}{
+		{"test@gmail.com", "testname"},
+		{"test2@gmail.com", ""},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			users.mu.Lock()
+			users.storage[test.email] = test.name
+			users.mu.Unlock()
+
+			DeleteUser(test.email)
+
+			users.mu.Lock()
+			_, ok := users.storage[test.email]
+			users.mu.Unlock()
+
+			require.False(t, ok)
+		})
+	}
+}
